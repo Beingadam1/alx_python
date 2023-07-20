cfa4c5b9f88516546ea39538fee94856dcaf8841
Babatunde Adam <adamlanre224@gmail.com>
2023-07-20 18:13:01 +0100

    comment out print

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -21,7 +21,7 @@ def validate_password(password):
 
 
 
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+# print(validate_password("Password123"))
+# print(validate_password("abc123"))
+# print(validate_password("Password 123"))
+# print(validate_password("password123"))
