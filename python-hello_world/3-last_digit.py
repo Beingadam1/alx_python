b7a3727be0beda32b15b5ac00372623220bacc55
Babatunde Adam <adamlanre224@gmail.com>
2023-07-20 15:59:57 +0100

    checker error fix

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,14 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
+
 if last_num > 5:
     print('Last digit of {} is {} and is greater than 5'.format(number, last_num))
-elif last_num == 0:
-    print('Last digit of {} is {} and is 0'.format(number, last_num))
 elif last_num < 6 and number < 0:
     print('Last digit of {} is -{} and is less than 6 and not 0'.format(number, last_num))
+elif last_num == 0:
+    print('Last digit of {} is {} and is 0'.format(number, last_num))
 elif last_num > 5 and number < 0:
     print('Last digit of {} is -{} and is less than 6 and not 0'.format(number, last_num))
 else:
     print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last_num))
-    
