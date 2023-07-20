e781e6817fe94ebdccad92af915b38ea58f73e22
Babatunde Adam <adamlanre224@gmail.com>
2023-07-20 17:46:26 +0100

    comment out the print test

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -12,7 +12,7 @@ def fibonacci_sequence(n):
           fib.append(next_num)
     return fib
 
-print(fibonacci_sequence(6))
-print(fibonacci_sequence(1))
-print(fibonacci_sequence(0))
-print(fibonacci_sequence(20))
+# print(fibonacci_sequence(6))
+# print(fibonacci_sequence(1))
+# print(fibonacci_sequence(0))
+# print(fibonacci_sequence(20))
