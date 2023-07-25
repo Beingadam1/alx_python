bdfdad71efcd2285e4a0ed156f2549cb94d66191
Babatunde Adam <adamlanre224@gmail.com>
2023-07-25 21:11:02 +0100

    task 5

---
+++ python-import_modules/5-main.py
@@ -0,0 +1,6 @@
+raise_exception_msg = __import__("5-raise_exception_msg").raise_exception_msg
+
+try:
+    raise_exception_msg("")
+except NameError as ne:
+    print(ne)
---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,2 @@
+def raise_exception_msg(message=""):
+    raise NameError(message or "This is a name exception.")
