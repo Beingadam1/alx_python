4ddd12a96b791c7d814711171536ba05865a0826
Babatunde Adam <adamlanre224@gmail.com>
2023-07-29 14:43:25 +0100

    function that returns a list with all values multiplied by a number without using any loops

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,10 +3,10 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    # use max to find the highest value and return None if it empty
+    # use max to find the highest value and return None if it empty.
     b_score = max(a_dictionary.values(), default=None)
 
-    # loop over the dict to check if the current iteration is equal to b_score
+    # loop over the dict to check if the current iteration is equal to b_score and return the key.
     for key, value in a_dictionary.items():
         if value == b_score:
             return key
