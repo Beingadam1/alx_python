e85299ce6942b2aa1f4b02b867714592b5a54e4a
Babatunde Adam <adamlanre224@gmail.com>
2023-07-29 14:40:48 +0100

    task 3

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,12 @@
+def best_score(a_dictionary):
+    # check if the dict is empty, return None if yes.
+    if a_dictionary is None:
+        return None
+
+    # use max to find the highest value and return None if it empty
+    b_score = max(a_dictionary.values(), default=None)
+
+    # loop over the dict to check if the current iteration is equal to b_score
+    for key, value in a_dictionary.items():
+        if value == b_score:
+            return key
