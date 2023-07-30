1532b8b0ab9a8b2869bb7ab21771583164f418c7
Babatunde Adam <adamlanre224@gmail.com>
2023-07-30 22:48:52 +0100

    Document the module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,8 @@
 class Square:
-    '''class Square that defines a square by:
-
-    Private instance attribute: size
-    Instantiation with size (no type/value verification)
-    You are not allowed to import any module'''
+    '''Represents a square'''
 
     def __init__(self, size) -> None:
+        '''Initializing this square class
+        Args: size - represnets the size of the square defined
+        '''
         self.__size = size
-
-
-Square1 = Square(6)
