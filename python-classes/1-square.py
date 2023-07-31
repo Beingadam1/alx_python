73a9f71d79e796a6aaca55dcac08e6b15d71e676
Babatunde Adam <adamlanre224@gmail.com>
2023-07-31 09:58:52 +0100

    Square that defines a square Instantiation  and raises error

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+class Square:
+    '''Represents a square'''
+
+    def __init__(self, size=0) -> None:
+        '''Initializing this square class
+        Args: size - represnets the size of the square defined
+        Raises: TypeError: if size is not integer
+                ValueError: if size is less than zero
+        '''
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
