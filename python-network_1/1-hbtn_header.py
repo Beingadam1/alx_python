07783d31fa085b74c703d58c35a7ab850d83e4a4
Babatunde Adam <adamlanre224@gmail.com>
2023-08-12 17:27:24 +0100

    add comments

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,13 +3,15 @@ import sys
 
 
 def main():
-    url = sys.argv[1]
-    req = requests.get(url)
+    url = sys.argv[1]  # Get the URL from the command-line argument
+    req = requests.get(url)  # Send an HTTP GET request to the URL
+
+    # Check if the 'X-Request-Id' header is present in the response headers
     if 'X-Request-Id' in req.headers:
-        reqValue = req.headers['X-Request-Id']
-        print(reqValue)
+        reqValue = req.headers['X-Request-Id']  # Get the value of 'X-Request-Id' header
+        print(reqValue)  # Print the value of 'X-Request-Id' header
     else:
-        print("None")
+        print('None')  # If 'X-Request-Id' header is not present, print "None"
 
 
 if __name__ == '__main__':
