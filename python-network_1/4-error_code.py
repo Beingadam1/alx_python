247bb707b176239f0ee5b63e59bc91b17f84edf8
Babatunde Adam <adamlanre224@gmail.com>
2023-08-12 17:47:39 +0100

    add comments

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -3,13 +3,14 @@ import sys
 
 
 def main():
-    url = sys.argv[1]
-    req = requests.get(url)
+    url = sys.argv[1]  # Get the URL from the command-line argument
+    req = requests.get(url)  # Send an HTTP GET request to the URL
 
+    # Check if the status code of the req is >= 400
     if req.status_code >= 400:
-        print(f'Error code: {req.status_code}')
+        print(f'Error code: {req.status_code}')  # prints Error otherwise print the body
     else:
-        print(req.text)
+        print(req.text)  # Print the body
 
 
 if __name__ == '__main__':
