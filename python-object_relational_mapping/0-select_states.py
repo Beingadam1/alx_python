5d00c72bc2c3e6de9b8dbe96c3a1312868396b00
Babatunde Adam <adamlanre224@gmail.com>
2023-08-24 13:19:43 +0100

    add ASC to the query

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
-# Write a script that lists all states from the database hbtn_0e_0_usa:
-# Your script should take 3 arguments: mysql username, mysql password
-# and database name (no argument validation needed)
-# You must use the module MySQLdb (import MySQLdb)
-# Your script should connect to a MySQL server running on localhost at port 3306
-# Results must be sorted in ascending order by states.id
-# Results must be displayed as they are in the example below
-# Your code should not be executed when imported
-
+'''
+Write a script that lists all states from the database hbtn_0e_0_usa:
+Your script should take 3 arguments: mysql username, mysql password
+and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+'''
 
 # Import modules needed
 import MySQLdb
@@ -31,7 +32,7 @@ if __name__ == '__main__':
 
     # Tackle task
     # create query
-    query = 'SELECT * FROM state ORDER BY id'
+    query = 'SELECT * FROM state ORDER BY id ASC'
 
     # execute query with cursor
     cur.execute(query)
