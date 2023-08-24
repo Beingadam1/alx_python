c82ba47f6de3e9a5e6c16a1c365672ec246958a0
Babatunde Adam <adamlanre224@gmail.com>
2023-08-24 14:16:41 +0100

    pep style

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         password = argv[2]
         db_name = argv[3]
         engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, db_name),
+            f'mysql+mysqldb://{user}:{password}@localhost/{db_name}',
             pool_pre_ping=True,
         )
         Session = sessionmaker(bind=engine)
